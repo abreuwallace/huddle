054eb93c4cd33acf623f4ad409eb0115e794009e
Luís Mourão <luis@phygitall.rio>
2021-04-18 12:07:53 -0300

    huddle start - Página de Login

---
+++ src/App.js
@@ -0,0 +1,8 @@
+import Login from './login/Login'
+import 'semantic-ui-css/semantic.min.css'
+
+function App() {
+  return <Login />
+}
+
+export default App;
---
+++ src/login/Login.js
@@ -0,0 +1,39 @@
+import React from 'react'
+// import React, { useState } from 'react'
+import { Button, Form, Grid, Header, Icon, Segment } from 'semantic-ui-react'
+
+const Login = () => {
+    // const [user, setUser] = useState('')
+
+    return (
+        <Grid textAlign='center' style={{ height: '100vh' }} verticalAlign='middle'>
+            <Grid.Column style={{ maxWidth: 450 }}>
+                <Header as='h2' color='teal' textAlign='center'>
+                    {/* <Image src='/logo.png' />  */}
+                    <Icon name='users' color='teal' />
+                    Huddle
+                </Header>
+                <Form size='large'>
+                    <Segment stacked>
+                        <Form.Input fluid icon='user' iconPosition='left' placeholder='E-mail de Usuário' />
+                        <Form.Input
+                            fluid
+                            icon='lock'
+                            iconPosition='left'
+                            placeholder='Senha'
+                            type='password'
+                        />
+                        <Button color='teal' fluid size='large'>
+                            Login
+                        </Button>
+                    </Segment>
+                </Form>
+                {/* <Message>
+                    Novo Usuário? <a href='#'>Inscreva-se</a>
+                </Message> */}
+            </Grid.Column>
+        </Grid>
+    )
+}
+
+export default Login
